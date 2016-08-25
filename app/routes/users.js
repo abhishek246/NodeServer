5e147c77d3a10e95b83940bbb6bc3fe918293422
Abhishek K <abhishek.k6006@gmail.com>
2016-08-25 22:20:36 +0530

    node Js server code added

---
+++ app/routes/users.js
@@ -0,0 +1,30 @@
+// app/routes/users.js
+
+var logger = require('winston');
+
+module.exports = function(router) {
+  'use strict';
+  // This will handle the url calls for /users/:user_id
+  router.route('/:userId')
+  .get(function(req, res, next) {
+    logger.info('[APP] GET  lol');
+    res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+  })
+  .put(function(req, res, next) {
+    // Update user
+  })
+  .patch(function(req, res,next) {
+    // Patch
+  })
+  .delete(function(req, res, next) {
+    // Delete record
+  });
+
+  router.route('/')
+  .get(function(req, res, next) {
+    // Logic for GET /users routes
+    res.json({notes: "Root Folder"})
+  }).post(function(req, res, next) {
+    // Create new user
+  });
+};

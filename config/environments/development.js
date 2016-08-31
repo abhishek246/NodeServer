4b616c98a8a2a1ee675372d9fe9a56fa65951d53
Abhishek K <abhishek.k6006@gmail.com>
2016-08-31 17:10:49 +0530

    Basic api for Node js

--- config/environments/development.js
+++ config/environments/development.js
@@ -6,3 +6,11 @@ nconf.set('database', {
   password: 'password',
   server: 'url'
 });
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'Happay_v2'
+});
